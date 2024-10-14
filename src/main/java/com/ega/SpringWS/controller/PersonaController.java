786b78b0560981efcbd320c54a62597ab34f348b
Wishmaster-sa <ich.danyliuk@gmail.com>
2024-10-14 11:17:08 +0200

    Update PersonaController.java

--- src/main/java/com/ega/SpringWS/controller/PersonaController.java
+++ src/main/java/com/ega/SpringWS/controller/PersonaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -144,7 +144,7 @@ public class PersonaController {
 //Тут ми говоримо, що шлях http://localhost:8080/api/v1/persons буде використовуватись для оновлення користувача в базі даних.
 //зіставлення користувача буде виконуватись за ID
 //Параметри запиту повинні додаватись у тілі запиту в JSON форматі.
-    @PutMapping("update")
+    @PatchMapping("update")
    //Анотація @RequestBody говорить SPRINGу що тіло запиту треба перетворити з JSON в обʼєкт класу Persona
     public Answer updatePersona(@RequestBody Persona persona){
         Answer ans;
